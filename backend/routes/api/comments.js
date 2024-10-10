a28ed507862c989b1ba65160e4153143ad7b57c7
WilcoApp <hello@wilco.gg>
2024-10-10 11:47:36 +0000

    Initial commit

--- backend/routes/api/comments.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const mongoose = require("mongoose");
-const Comment = mongoose.model("Comment");
-
-// add another endpoint for deleting a comment
-
-
-
-module.exports = router;
